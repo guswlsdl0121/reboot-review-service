58d14c8cc460c7a7ab752b61cd2e3fbb8cd70166
íguswlsdl0121 <guswlsdl0121@gmail.com>
2024-10-09 16:52:34 +0900

    feat: 엔티티 작성
    - 요구사항에 맞게 Review, Product, User 생성
    - 기능명세 상품 테이블 명칭 item -> product로 변경
    - 유효성 검사를 위해 dependencies에 validation 추가

---
+++ src/main/java/com/reboot_course/review_service/entity/Review.java
@@ -0,0 +1,43 @@
+package com.reboot_course.review_service.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    @Min(0)
+    @Max(5)
+    private Integer score;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @Column
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    // 연관 관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
