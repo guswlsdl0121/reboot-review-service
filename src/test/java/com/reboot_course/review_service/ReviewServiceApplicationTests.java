8facc3c8c8ef812ca99596c40505a8ed6c4936b6
íguswlsdl0121 <guswlsdl0121@gmail.com>
2024-10-09 10:31:08 +0900

    build: Spring initializer로 프로젝트 최초 생성

---
+++ src/test/java/com/reboot_course/review_service/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reboot_course.review_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
